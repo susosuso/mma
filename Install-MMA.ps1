ced61efce3f0092cf2267006abc9ff2b67447f95
Sunnie So <suso@microsoft.com>
2019-10-10 02:41:09 -0700

    update

--- Install-MMA.ps1
+++ Install-MMA.ps1
@@ -7,6 +7,7 @@ $MMADownloadPath = "https://github.com/susosuso/mma/raw/master/MMASetup-AMD64.ex
 $mmaDir = "C:\MMA"
 $MMAexe = ".\MMASetup-AMD64.exe"
 $logfile = "C:\mma-install.log"
+Write-Output "workspaceId is $workspaceId" >> $logfile
 
 Write-Output "Downloading MMA from $MMADownloadPath as $MMAexe..." >> $logfile
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
@@ -16,7 +17,7 @@ Write-Output "Running $MMAexe ..." >> $logfile
 & $MMAexe /c /t:$mmaDir
 
 Set-Location $mmaDir
-Write-Output "Runnig setup.exe ..." >> $logfile
-& ./setup.exe /qn NOAPM=1 ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_AZURE_CLOUD_TYPE=0 OPINSIGHTS_WORKSPACE_ID="$workspaceId" OPINSIGHTS_WORKSPACE_KEY="$workspaceKey" AcceptEndUserLicenseAgreement=1  >> $logfile
+Write-Output "Runnig setup.exe ..." *>> $logfile
+& ./setup.exe /qn NOAPM=1 ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_AZURE_CLOUD_TYPE=0 OPINSIGHTS_WORKSPACE_ID="$workspaceId" OPINSIGHTS_WORKSPACE_KEY="$workspaceKey" AcceptEndUserLicenseAgreement=1  *>> $logfile
 
 Write-Output "Done." >> $logfile
