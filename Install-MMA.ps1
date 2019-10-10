16c1582d5a5cf6ff6b8b70af0b7b0a8a80e0dd7a
Sunnie So <suso@microsoft.com>
2019-10-10 09:13:58 -0700

    Fix

--- Install-MMA.ps1
+++ Install-MMA.ps1
@@ -16,8 +16,19 @@ Invoke-WebRequest -Uri $MMADownloadPath -OutFile $MMAexe -UseBasicParsing
 Write-Output "Running $MMAexe ..." >> $logfile
 & $MMAexe /c /t:$mmaDir
 
+Start-Sleep -Seconds 3
+
 Set-Location $mmaDir
+dir *>> $logfile 
 Write-Output "Runnig setup.exe ..." *>> $logfile
-& ./setup.exe /qn NOAPM=1 ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_AZURE_CLOUD_TYPE=0 OPINSIGHTS_WORKSPACE_ID="$workspaceId" OPINSIGHTS_WORKSPACE_KEY="$workspaceKey" AcceptEndUserLicenseAgreement=1  *>> $logfile
+& .\Setup.exe /qn NOAPM=1 ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_AZURE_CLOUD_TYPE=0 OPINSIGHTS_WORKSPACE_ID="$workspaceId" OPINSIGHTS_WORKSPACE_KEY="$workspaceKey" AcceptEndUserLicenseAgreement=1
+
+if($LASTEXITCODE -eq 0)
+{
+    write-output "setup.exe returns 0" >> $logfile
+}
+else{
+    write-output "setup.exe returns non zero" >> $logfile
+}
 
 Write-Output "Done." >> $logfile
